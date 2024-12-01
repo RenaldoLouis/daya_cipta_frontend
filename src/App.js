7e49da8a21a04e98a8632cc3dbdb565e667bdd20
Renaldo Louis <renaldo.louis@commsult.id>
2024-12-01 21:28:59 +0700

    import component from library succcess

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import { Button, CustomCombobox } from 'daya_cipta_erp/dist';
 import './App.css';
+import logo from './logo.svg';
+
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <CustomCombobox></CustomCombobox>
+        <Button label={"asdasdasdaasd"}></Button>
         <a
           className="App-link"
           href="https://reactjs.org"
